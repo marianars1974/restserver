da6aff59b8ac31cb104ce1fa477bc35d9d7cabe6
unknown <mrsebedio@gmail.com>
2020-04-11 08:47:38 -0300

    RestServer usaurio

--- server/server.js
+++ server/server.js
@@ -1,48 +1,34 @@
-require ("./config/config.js");
+require('./config/config');
+
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
+
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
- 
+app.use(bodyParser.urlencoded({ extended: false }))
+
 // parse application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
-  res.json({"nombre":"Mariana","edad":"47"})
-})
 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
+app.use(require('./routes/usuario'));
 
-    if (body.nombre ===undefined)
-    {
-        res.status(400).json({
-                ok: false,
-                mensaje: "el nombre es obligatorio"
-        });
-    }
-    else
-    {
-        res.json({
-            persona: body
-        });
-    }
-    let nombre =body.nombre;
-    let edad =body.edad;
-   
-   
-  });
- 
-app.put('/usuario/:id', function (req, res) {
-let id_usuario =req.params.id;
-res.json({id_usuario,"nombre":"Mariana","edad":"47"})
-})
 
-app.delete('/usuario', function (req, res) {
-    res.json({"nombre":"Mariana","edad":"47"})
-  })
-app.listen(process.env.PORT,()=>{
-    console.log("escuchado puerto",process.env.PORT);
-})
+
+mongoose.connect(process.env.URLDB,{useNewUrlParser:true, useCreateIndex: true}, (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
+
+});
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
